8b3a91d7762f09256c9a98597f935091db5ea54a
Alex <aleksander.gorka@gmail.com>
2018-12-17 10:13:13 +0100

    Initial commit

---
+++ Assets/VisualEffectController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class VisualEffectController : MonoBehaviour
+{
+    public VisualEffect ve;
+    public Transform leftHand;
+    public Transform rightHand;
+    
+    private void Update()
+    {
+        ve.transform.SetPositionAndRotation((leftHand.position+rightHand.position)/2f,Quaternion.identity);
+        ve.SetFloat("Radius", Vector3.Distance(leftHand.position,rightHand.position)/5f);
+    }
+}
