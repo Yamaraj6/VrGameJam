b8f89f6ce52a8213815d7dc908ff0edcaa7a5637
Alex <aleksander.gorka@gmail.com>
2018-12-17 13:52:14 +0100

    ADDED: planet rotation

---
+++ Assets/Scripts/RotationCopy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationCopy : MonoBehaviour
+{
+    public Transform transformCopy;
+    
+    void Update()
+    {
+        transformCopy.rotation = transform.rotation;
+    }
+}
