fb77f6795f33f6339110db0a67da9a145def8589
Alex <aleksander.gorka@gmail.com>
2018-12-18 16:19:39 +0100

    ADDED: hand gesture recognition

--- Assets/Scripts/Hands/GestureRecognition.cs
+++ Assets/Scripts/Hands/GestureRecognition.cs
@@ -1,20 +1,96 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class GestureRecognition : MonoBehaviour
 {
-    public Collider fistCollider;
+    [SerializeField] private Hand hand;
+    [SerializeField]private Collider fistCollider;
+    [SerializeField]private Collider straightFingerCollider;
+    [SerializeField]private Collider openHandCollider;
+    private Gesture actualGesture;
+   
+    private OVRInput.Axis1D indexTrigger;
+    private OVRInput.Axis1D handTrigger;
+
+    private void Awake()
+    {
+        switch (hand)
+        {
+            case Hand.Left:
+                indexTrigger = OVRInput.Axis1D.PrimaryIndexTrigger;
+                handTrigger = OVRInput.Axis1D.PrimaryHandTrigger;
+                break;
+            case Hand.Right:
+                indexTrigger = OVRInput.Axis1D.SecondaryIndexTrigger;
+                handTrigger = OVRInput.Axis1D.SecondaryHandTrigger;
+                break;
+        }
+    }
+
     void Update()
     {
-        Debug.Log(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) );
-        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) >0.7f && OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) >0.7f)
+        UpdateGesture();
+    }
+
+    private void UpdateGesture()
+    {
+        if (OVRInput.Get(handTrigger) > 0.9f)
         {
-            fistCollider.enabled = true;
+            if (OVRInput.Get(indexTrigger) > 0.9f)
+            {
+                ActualizeGesture(Gesture.Fist);
+            }
+            else
+            {
+                ActualizeGesture(Gesture.StraightFinger);
+            }
         }
         else
         {
-            fistCollider.enabled = false;
+            ActualizeGesture(Gesture.OpenHand);
         }
     }
+
+    private void ActualizeGesture(Gesture gesture)
+    {
+        if (actualGesture == gesture) return;
+        actualGesture = gesture;
+        switch (gesture)
+        {
+            case Gesture.Fist:
+                fistCollider.enabled = true;
+                straightFingerCollider.enabled = false;
+                openHandCollider.enabled = false;
+                break;
+            case Gesture.StraightFinger:
+                fistCollider.enabled = false;
+                straightFingerCollider.enabled = true;
+                openHandCollider.enabled = false;
+                break;
+            case Gesture.OpenHand:
+                fistCollider.enabled = false;
+                straightFingerCollider.enabled = false;
+                openHandCollider.enabled = true;
+                break;
+        }
+
+    }
+
+    public Gesture GetActualGesture()
+    {
+        return actualGesture;
+    }
+}
+
+public enum Gesture
+{
+    Fist,
+    StraightFinger,
+    OpenHand
+}
+
+public enum Hand
+{
+    Left,
+    Right
 }
