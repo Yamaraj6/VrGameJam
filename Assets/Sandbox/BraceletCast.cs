3439b8f4247a0e062150c1e02d207d64053ea6e5
dawidwitulski <dawid@thedust.pl>
2018-12-18 15:50:48 +0100

    ADDED: Bracelet cast triggers

---
+++ Assets/Sandbox/BraceletCast.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BraceletCast : MonoBehaviour
+{
+    public UnityEvent skillToCast;
+    
+    private void OnTriggerEnter(Collider other) { 
+        if (other.CompareTag("StraightFinger"))
+        {
+            skillToCast.Invoke();
+        }
+    }
+}
